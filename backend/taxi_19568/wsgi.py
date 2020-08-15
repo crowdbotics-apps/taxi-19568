3be8af2d655f392a7587205df8b6cf0bf5767c00
Crowdbotics <team@crowdbotics.com>
2020-08-15 01:19:22 +0000

    Update models

--- backend/taxi_19568/wsgi.py
+++ backend/taxi_19568/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taxi_19568.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taxi_19568.settings")
 
 application = get_wsgi_application()
